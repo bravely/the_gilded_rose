b850503c49927152ff4547fc1c3a6661d9a1a237
Jake Demarest-Mays <jake@demarest-mays.com>
2023-04-25 16:50:26 -0600

    DRY'd the tests for the sake of readability/clarity

--- test/gilded_rose_test.exs
+++ test/gilded_rose_test.exs
@@ -14,58 +14,89 @@ defmodule GildedRoseTest do
     test "standard items before their sell date decrease their quality and sell_in by 1" do
       gilded_rose = GildedRose.new()
 
-      items = GildedRose.items(gilded_rose)
-      vest_index = Enum.find_index(items, &(&1.name == "+5 Dexterity Vest"))
-      %Item{name: "+5 Dexterity Vest", sell_in: 10, quality: 20} = Enum.at(items, vest_index)
-      elixir_index = Enum.find_index(items, &(&1.name == "Elixir of the Mongoose"))
-      %Item{name: "Elixir of the Mongoose", sell_in: 5, quality: 7} = Enum.at(items, elixir_index)
+      gilded_rose
+      |> GildedRose.items()
+      |> assert_item_matches(%Item{
+        name: "+5 Dexterity Vest",
+        sell_in: 10,
+        quality: 20
+      })
+      |> assert_item_matches(%Item{
+        name: "Elixir of the Mongoose",
+        sell_in: 5,
+        quality: 7
+      })
 
       assert :ok == GildedRose.update_quality(gilded_rose)
-      items = GildedRose.items(gilded_rose)
 
-      %GildedRose.Item{name: "+5 Dexterity Vest", sell_in: 9, quality: 19} =
-        Enum.at(items, vest_index)
-
-      %GildedRose.Item{name: "Elixir of the Mongoose", sell_in: 4, quality: 6} =
-        Enum.at(items, elixir_index)
+      gilded_rose
+      |> GildedRose.items()
+      |> assert_item_matches(%Item{
+        name: "+5 Dexterity Vest",
+        sell_in: 9,
+        quality: 19
+      })
+      |> assert_item_matches(%Item{
+        name: "Elixir of the Mongoose",
+        sell_in: 4,
+        quality: 6
+      })
     end
 
     test "standard items after their sell date decrease their quality by 2, sell_in by 1" do
       gilded_rose = GildedRose.new()
 
+      gilded_rose
+      |> GildedRose.items()
       # Baseline, and re-establishing sell_in and quality amounts while they decrease normally.
-      items = GildedRose.items(gilded_rose)
-
-      assert %Item{name: "+5 Dexterity Vest", sell_in: 10, quality: 20} =
-               Enum.find(items, &(&1.name == "+5 Dexterity Vest"))
-
-      assert %Item{name: "Elixir of the Mongoose", sell_in: 5, quality: 7} =
-               Enum.find(items, &(&1.name == "Elixir of the Mongoose"))
+      |> assert_item_matches(%Item{
+        name: "+5 Dexterity Vest",
+        sell_in: 10,
+        quality: 20
+      })
+      |> assert_item_matches(%Item{
+        name: "Elixir of the Mongoose",
+        sell_in: 5,
+        quality: 7
+      })
 
       # Work up to the sell_in date for the Elixir of the Mongoose.
       for _ <- 1..5 do
         assert :ok == GildedRose.update_quality(gilded_rose)
       end
 
-      items = GildedRose.items(gilded_rose)
-
-      assert %Item{name: "+5 Dexterity Vest", sell_in: 5, quality: 15} =
-               Enum.find(items, &(&1.name == "+5 Dexterity Vest"))
-
-      assert %Item{name: "Elixir of the Mongoose", sell_in: 0, quality: 2} =
-               Enum.find(items, &(&1.name == "Elixir of the Mongoose"))
+      gilded_rose
+      |> GildedRose.items()
+      # Baseline, and re-establishing sell_in and quality amounts while they decrease normally.
+      |> assert_item_matches(%Item{
+        name: "+5 Dexterity Vest",
+        sell_in: 5,
+        quality: 15
+      })
+      |> assert_item_matches(%Item{
+        name: "Elixir of the Mongoose",
+        sell_in: 0,
+        quality: 2
+      })
 
       # Work past the sell_in date for the Elixir of the Mongoose, where quality
       # decreases twice as fast, but the Vest decreases normally.
 
       assert :ok == GildedRose.update_quality(gilded_rose)
-      items = GildedRose.items(gilded_rose)
 
-      assert %Item{name: "+5 Dexterity Vest", sell_in: 4, quality: 14} =
-               Enum.find(items, &(&1.name == "+5 Dexterity Vest"))
-
-      assert %Item{name: "Elixir of the Mongoose", sell_in: -1, quality: 0} =
-               Enum.find(items, &(&1.name == "Elixir of the Mongoose"))
+      gilded_rose
+      |> GildedRose.items()
+      # Baseline, and re-establishing sell_in and quality amounts while they decrease normally.
+      |> assert_item_matches(%Item{
+        name: "+5 Dexterity Vest",
+        sell_in: 4,
+        quality: 14
+      })
+      |> assert_item_matches(%Item{
+        name: "Elixir of the Mongoose",
+        sell_in: -1,
+        quality: 0
+      })
 
       # Work up to the sell_in date for the Vest. Elixir's quality shouldn't
       # decrease past 0.
@@ -73,152 +104,226 @@ defmodule GildedRoseTest do
         assert :ok == GildedRose.update_quality(gilded_rose)
       end
 
-      items = GildedRose.items(gilded_rose)
-
-      assert %Item{name: "+5 Dexterity Vest", sell_in: 0, quality: 10} =
-               Enum.find(items, &(&1.name == "+5 Dexterity Vest"))
-
-      assert %Item{name: "Elixir of the Mongoose", sell_in: -5, quality: 0} =
-               Enum.find(items, &(&1.name == "Elixir of the Mongoose"))
+      gilded_rose
+      |> GildedRose.items()
+      # Baseline, and re-establishing sell_in and quality amounts while they decrease normally.
+      |> assert_item_matches(%Item{
+        name: "+5 Dexterity Vest",
+        sell_in: 0,
+        quality: 10
+      })
+      |> assert_item_matches(%Item{
+        name: "Elixir of the Mongoose",
+        sell_in: -5,
+        quality: 0
+      })
 
       # Work past Vest sell_in, ensuring quality decreases twice as fast.
 
       assert :ok == GildedRose.update_quality(gilded_rose)
-      items = GildedRose.items(gilded_rose)
 
-      assert %Item{name: "+5 Dexterity Vest", sell_in: -1, quality: 8} =
-               Enum.find(items, &(&1.name == "+5 Dexterity Vest"))
-
-      assert %Item{name: "Elixir of the Mongoose", sell_in: -6, quality: 0} =
-               Enum.find(items, &(&1.name == "Elixir of the Mongoose"))
+      gilded_rose
+      |> GildedRose.items()
+      # Baseline, and re-establishing sell_in and quality amounts while they decrease normally.
+      |> assert_item_matches(%Item{
+        name: "+5 Dexterity Vest",
+        sell_in: -1,
+        quality: 8
+      })
+      |> assert_item_matches(%Item{
+        name: "Elixir of the Mongoose",
+        sell_in: -6,
+        quality: 0
+      })
     end
 
     test "Aged Brie increases in quality as its sell_in decreases, but never exceeds maximum" do
       gilded_rose = GildedRose.new()
 
+      gilded_rose
+      |> GildedRose.items()
       # Baseline Aged Brie sell_in and quality.
-      items = GildedRose.items(gilded_rose)
-
-      assert %Item{name: "Aged Brie", sell_in: 2, quality: 0} =
-               Enum.find(items, &(&1.name == "Aged Brie"))
+      |> assert_item_matches(%Item{
+        name: "Aged Brie",
+        sell_in: 2,
+        quality: 0
+      })
 
       # Work up to the sell_in date for the Aged Brie. Quality should increase by 1 each time.
       for _ <- 1..2 do
         assert :ok == GildedRose.update_quality(gilded_rose)
       end
 
-      items = GildedRose.items(gilded_rose)
-
-      assert %Item{name: "Aged Brie", sell_in: 0, quality: 2} =
-               Enum.find(items, &(&1.name == "Aged Brie"))
+      gilded_rose
+      |> GildedRose.items()
+      # Baseline Aged Brie sell_in and quality.
+      |> assert_item_matches(%Item{
+        name: "Aged Brie",
+        sell_in: 0,
+        quality: 2
+      })
 
       # Past sell_in, quality should increase by 2 each time...
       for _ <- 1..24 do
         assert :ok == GildedRose.update_quality(gilded_rose)
       end
 
-      items = GildedRose.items(gilded_rose)
-
-      assert %Item{name: "Aged Brie", sell_in: -24, quality: 50} =
-               Enum.find(items, &(&1.name == "Aged Brie"))
+      gilded_rose
+      |> GildedRose.items()
+      |> assert_item_matches(%Item{
+        name: "Aged Brie",
+        sell_in: -24,
+        quality: 50
+      })
 
       # ...but can't exceed 50.
       for _ <- 1..2 do
         assert :ok == GildedRose.update_quality(gilded_rose)
       end
 
-      items = GildedRose.items(gilded_rose)
-
-      assert %Item{name: "Aged Brie", sell_in: -26, quality: 50} =
-               Enum.find(items, &(&1.name == "Aged Brie"))
+      gilded_rose
+      |> GildedRose.items()
+      |> assert_item_matches(%Item{
+        name: "Aged Brie",
+        sell_in: -26,
+        quality: 50
+      })
     end
 
     test "Sulfuras never decreases in quality as its sell_in increases" do
       gilded_rose = GildedRose.new()
 
-      items = GildedRose.items(gilded_rose)
-
+      gilded_rose
+      |> GildedRose.items()
       # Baseline for Sulfuras.
-      assert %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 0, quality: 80} =
-               Enum.find(items, &(&1.name == "Sulfuras, Hand of Ragnaros"))
+      |> assert_item_matches(%Item{
+        name: "Sulfuras, Hand of Ragnaros",
+        sell_in: 0,
+        quality: 80
+      })
 
       # Work up to the sell_in date for the Sulfuras. Quality should never change.
       for _ <- 1..10 do
         assert :ok == GildedRose.update_quality(gilded_rose)
       end
 
-      items = GildedRose.items(gilded_rose)
-
-      assert %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 0, quality: 80} =
-               Enum.find(items, &(&1.name == "Sulfuras, Hand of Ragnaros"))
+      gilded_rose
+      |> GildedRose.items()
+      # Still baseline.
+      |> assert_item_matches(%Item{
+        name: "Sulfuras, Hand of Ragnaros",
+        sell_in: 0,
+        quality: 80
+      })
     end
 
     test "Backstage passes increases in quality according to its own rules" do
       gilded_rose = GildedRose.new()
 
-      items = GildedRose.items(gilded_rose)
-
+      gilded_rose
+      |> GildedRose.items()
       # Baseline for Backstage passes.
-      assert %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 15, quality: 20} =
-               Enum.find(items, &(&1.name == "Backstage passes to a TAFKAL80ETC concert"))
+      |> assert_item_matches(%Item{
+        name: "Backstage passes to a TAFKAL80ETC concert",
+        sell_in: 15,
+        quality: 20
+      })
 
       # Work up to 10 days before, when quality increases by 1 each time.
       for _ <- 1..5 do
         assert :ok == GildedRose.update_quality(gilded_rose)
       end
 
-      items = GildedRose.items(gilded_rose)
-
-      assert %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 10, quality: 25} =
-               Enum.find(items, &(&1.name == "Backstage passes to a TAFKAL80ETC concert"))
+      gilded_rose
+      |> GildedRose.items()
+      |> assert_item_matches(%Item{
+        name: "Backstage passes to a TAFKAL80ETC concert",
+        sell_in: 10,
+        quality: 25
+      })
 
       # Under 10 days, but more than 5, quality increases by 2 each time.
       assert :ok == GildedRose.update_quality(gilded_rose)
-      items = GildedRose.items(gilded_rose)
 
-      assert %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 9, quality: 27} =
-               Enum.find(items, &(&1.name == "Backstage passes to a TAFKAL80ETC concert"))
+      gilded_rose
+      |> GildedRose.items()
+      |> assert_item_matches(%Item{
+        name: "Backstage passes to a TAFKAL80ETC concert",
+        sell_in: 9,
+        quality: 27
+      })
 
       for _ <- 1..4 do
         assert :ok == GildedRose.update_quality(gilded_rose)
       end
 
-      items = GildedRose.items(gilded_rose)
-
-      assert %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 5, quality: 35} =
-               Enum.find(items, &(&1.name == "Backstage passes to a TAFKAL80ETC concert"))
+      gilded_rose
+      |> GildedRose.items()
+      |> assert_item_matches(%Item{
+        name: "Backstage passes to a TAFKAL80ETC concert",
+        sell_in: 5,
+        quality: 35
+      })
 
       # Now, under 5 days, quality increases by 3 each time.
       assert :ok == GildedRose.update_quality(gilded_rose)
-      items = GildedRose.items(gilded_rose)
 
-      assert %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 4, quality: 38} =
-               Enum.find(items, &(&1.name == "Backstage passes to a TAFKAL80ETC concert"))
+      gilded_rose
+      |> GildedRose.items()
+      |> assert_item_matches(%Item{
+        name: "Backstage passes to a TAFKAL80ETC concert",
+        sell_in: 4,
+        quality: 38
+      })
 
       for _ <- 1..4 do
         assert :ok == GildedRose.update_quality(gilded_rose)
       end
 
-      items = GildedRose.items(gilded_rose)
-
-      assert %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 0, quality: 50} =
-               Enum.find(items, &(&1.name == "Backstage passes to a TAFKAL80ETC concert"))
+      gilded_rose
+      |> GildedRose.items()
+      |> assert_item_matches(%Item{
+        name: "Backstage passes to a TAFKAL80ETC concert",
+        sell_in: 0,
+        quality: 50
+      })
 
       # And after the concert, quality drops to 0, and stays there.
       assert :ok == GildedRose.update_quality(gilded_rose)
-      items = GildedRose.items(gilded_rose)
 
-      assert %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: -1, quality: 0} =
-               Enum.find(items, &(&1.name == "Backstage passes to a TAFKAL80ETC concert"))
+      gilded_rose
+      |> GildedRose.items()
+      |> assert_item_matches(%Item{
+        name: "Backstage passes to a TAFKAL80ETC concert",
+        sell_in: -1,
+        quality: 0
+      })
 
       for _ <- 1..10 do
         assert :ok == GildedRose.update_quality(gilded_rose)
       end
 
-      items = GildedRose.items(gilded_rose)
-
-      assert %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: -11, quality: 0} =
-               Enum.find(items, &(&1.name == "Backstage passes to a TAFKAL80ETC concert"))
+      gilded_rose
+      |> GildedRose.items()
+      |> assert_item_matches(%Item{
+        name: "Backstage passes to a TAFKAL80ETC concert",
+        sell_in: -11,
+        quality: 0
+      })
     end
+  end
+
+  defp assert_item_matches(item_list, %Item{
+         name: name,
+         sell_in: sell_in,
+         quality: quality
+       }) do
+    # Ensuring we're not mistakenly matching a different item, by asserting there
+    # should only be one of any kind of item in the list.
+    assert [item] = Enum.filter(item_list, &(&1.name == name))
+    assert %Item{sell_in: ^sell_in, quality: ^quality} = item
+
+    # Return the list so we can pipe chain if we like!
+    item_list
   end
 end
